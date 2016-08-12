69c47a0e4a6100edbff6ec122c3d884175854db5
Xianda Long <kelvinlong8897@gmail.com>
2016-08-12 12:15:16 -0400

    add numbers activity

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,30 @@
+package com.example.android.miwok;
+
+/**
+ * Created by kelvin on 8/12/2016.
+ */
+public class Word {
+    private String defaultTranslation;
+    private String miwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        this.defaultTranslation = defaultTranslation;
+        this.miwokTranslation = miwokTranslation;
+    }
+
+    public String getDefaultTranslation() {
+        return defaultTranslation;
+    }
+
+    public void setDefaultTranslation(String defaultTranslation) {
+        this.defaultTranslation = defaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return miwokTranslation;
+    }
+
+    public void setMiwokTranslation(String miwokTranslation) {
+        this.miwokTranslation = miwokTranslation;
+    }
+}
---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,50 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kelvin on 8/12/2016.
+ */
+public class WordAdapter extends ArrayAdapter<Word> {
+    //constructor
+    public WordAdapter(Context context, ArrayList<Word> words) {
+        super(context, 0, words);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link AndroidFlavor} object located at this position in the list
+        Word word = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView enTextView = (TextView) listItemView.findViewById(R.id.en_word);
+        // Get the version name from the current AndroidFlavor object and
+        // set this text on the name TextView
+        enTextView.setText(word.getDefaultTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_word);
+        // Get the version number from the current AndroidFlavor object and
+        // set this text on the number TextView
+        miwokTextView.setText(word.getMiwokTranslation());
+
+
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
+    }
+}
