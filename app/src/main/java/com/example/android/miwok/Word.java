87507b004c21fd1bd7617be20e7c48fa0c9ecce2
Xianda Long <kelvinlong8897@gmail.com>
2016-08-12 16:27:39 -0400

    add pictures to each entry

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,12 +4,20 @@ package com.example.android.miwok;
  * Created by kelvin on 8/12/2016.
  */
 public class Word {
+    private static final int NO_IMAGE_PROVIDED = -1;
     private String defaultTranslation;
     private String miwokTranslation;
+    private int imageResourceId = NO_IMAGE_PROVIDED;
 
     public Word(String defaultTranslation, String miwokTranslation) {
         this.defaultTranslation = defaultTranslation;
         this.miwokTranslation = miwokTranslation;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        this.defaultTranslation = defaultTranslation;
+        this.miwokTranslation = miwokTranslation;
+        this.imageResourceId = imageResourceId;
     }
 
     public String getDefaultTranslation() {
@@ -26,5 +34,17 @@ public class Word {
 
     public void setMiwokTranslation(String miwokTranslation) {
         this.miwokTranslation = miwokTranslation;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public void setImageResourceId(int imageResourceId) {
+        this.imageResourceId = imageResourceId;
+    }
+
+    public boolean hasImage() {
+        return this.imageResourceId != NO_IMAGE_PROVIDED;
     }
 }
