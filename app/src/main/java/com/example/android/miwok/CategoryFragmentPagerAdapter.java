786b70b1b3f7124e392a6d3572888c7421de43a1
Xianda Long <kelvinlong8897@gmail.com>
2016-08-16 12:09:47 -0400

    add tabs for fragments

--- app/src/main/java/com/example/android/miwok/CategoryFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryFragmentPagerAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -8,8 +9,26 @@ import android.support.v4.app.FragmentPagerAdapter;
  * Created by kelvin on 8/16/2016.
  */
 public class CategoryFragmentPagerAdapter extends FragmentPagerAdapter {
-    public CategoryFragmentPagerAdapter(FragmentManager fm) {
+    Context mContext;
+
+    public CategoryFragmentPagerAdapter(Context context, FragmentManager fm) {
         super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
+
     }
 
     @Override
